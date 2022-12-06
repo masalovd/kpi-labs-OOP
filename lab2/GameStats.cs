129a48dc50b34a605249d5350f11429fe4b75bb6
masalovd <99145185+masalovd@users.noreply.github.com>
2022-12-06 22:44:57 +0200

    Add files via upload
    release v2

---
+++ lab2/GameStats.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameSimulator2
+{
+    class GameStats
+    {
+        public string OpponentName { get; set; }
+        public string Result { get; set; }
+        public int Rating { get; set; }
+
+        public string GameType { get; set; }
+
+
+        public GameStats(string opponentName, string gameType, string result, int rating)
+        {
+            OpponentName = opponentName;
+            GameType = gameType;
+            Result = result;
+            Rating = rating;
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using GameSimulator2;
+using System;
+
+
+namespace GameSimulator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StandartGame game1 = new StandartGame("Dice");
+            TrainingGame game2 = new TrainingGame("Cards");
+            OneWayGame game3 = new OneWayGame("2048");
+
+            GameAccount player1 = new GameAccount("Jack12345678", "base");
+            GameAccount player2 = new GameAccount("Ben123456789", "gold");
+
+            game1.PlayTheGame(player1, player2, 15);
+            game1.PlayTheGame(player1, player2, 10);
+            game1.PlayTheGame(player1, player2, 8);
+            game1.PlayTheGame(player1, player2, 5);
+            game2.PlayTheGame(player1, player2, 0);
+            game3.PlayTheGame(player1, player1, 2);
+            game3.PlayTheGame(player1, player1, 5);
+
+            Console.WriteLine("");
+            Console.WriteLine(player1.GetStatus());
+            Console.WriteLine("");
+            Console.WriteLine(player2.GetStatus());
+        }
+    }
+}
+
